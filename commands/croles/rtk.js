9b8635c42f65ed6359d196c2c807a21002477a5a
da-the-dev <leshiktk@me.com>
2021-05-21 14:41:53 +0300

    Reviving custom roles part 1

--- commands/croles/rtk.js
+++ commands/croles/rtk.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 const utl = require('../../utility')
-const sMsg = 'Конфискация кастомной роли'
+const { getConnection, DBUser, DBServer } = utl.db
+const sMsg = 'Выдача кастомной роли'
 module.exports =
     /**
     * @param {Array<string>} args Command argument
@@ -8,52 +9,46 @@ module.exports =
     * @param {Discord.Client} client Discord client object
     * @description Usage: .rgv <member> <rolePos>
     */
-    (args, msg, client) => {
+    async (args, msg, client) => {
         var mMember = msg.mentions.members.first()
-        if(!args[2] || !args[2][0] == 'c' || !Number.isInteger(Number(args[2].slice(1)))) {
-            utl.embed.ping(msg, sMsg, 'указан неверный индекс роли!')
-            return
-        }
-        var pos = args[2].slice(1)
         if(!mMember) {
             utl.embed.ping(msg, sMsg, 'не указан пользователь!')
             return
         }
-        if(!pos) {
+        if(!args[2]) {
             utl.embed.ping(msg, sMsg, 'не указана роль!')
             return
         }
+        if(args[2][0] != 'c' || !Number.isInteger(Number(args[2].slice(1)))) {
+            utl.embed.ping(msg, sMsg, 'указан неверный индекс роли!')
+            return
+        }
+        const pos = args[2].slice(1)
 
-        utl.db.createClient(process.env.MURL).then(async db => {
-            var userData = await db.get(msg.guild.id, msg.author.id)
-            if(!userData || !userData.customInv || !userData.customInv[pos - 1]) {
-                utl.embed.ping(msg, sMsg, 'у Вас нет кастомных ролей')
-                db.close()
-                return
-            }
+        const user = await new DBUser(msg.guild.id, msg.author.id, getConnection())
+        if(!user.customInv) {
+            utl.embed.ping(msg, sMsg, 'у Вас нет кастомных ролей')
+            return
+        }
 
-            var serverData = await db.getServer(msg.guild.id)
-            var role = serverData.customRoles.find(r => r.id == userData.customInv[pos - 1])
-            if(!role) {
-                utl.embed.ping(msg, sMsg, 'эта роль Вам не принадлежит!')
-                db.close()
-                return
-            }
+        const server = await new DBServer(msg.guild.id, getConnection())
+        const role = server.customRoles.find(r => r.id == user.customInv[pos - 1] && r.owner == msg.author.id)
+        if(!role) {
+            utl.embed.ping(msg, sMsg, 'эта роль Вам не принадлежит!')
+            return
+        }
 
-            var recipientData = await db.get(msg.guild.id, mMember.id)
-            if(!recipientData.customInv || !recipientData.customInv.find(r => r == role.id)) {
-                utl.embed.ping(msg, sMsg, `этой роли нет у <@${mMember.id}>!`)
-                db.close()
-                return
-            }
+        const recipient = await new DBUser(msg.guild.id, mMember.id, getConnection())
+        if(!recipient.customInv || !recipient.customInv.find(r => r == role.id)) {
+            utl.embed.ping(msg, sMsg, `этой роли нет у <@${mMember.id}>!`)
+            return
+        }
 
-            serverData.customRoles[serverData.customRoles.findIndex(r => r.id == role.id && r.owner == msg.author.id)].members -= 1
-            if(mMember.roles.cache.has(role.id))
-                mMember.roles.remove(role.id)
-            utl.embed.ping(msg, sMsg, `роль <@&${role.id}> была забрана у <@${mMember.id}>`)
+        server.customRoles.find(r => r.id).members--
+        server.save()
+        recipient.customInv.splice(recipient.customInv.indexOf(r => r.id == role.id), 1)
+        mMember.roles.remove(role.id)
+        recipient.save()
 
-            db.update(msg.guild.id, mMember.id, { $pull: { customInv: role.id } }).then(() => {
-                db.setServer(msg.guild.id, serverData).then(() => db.close())
-            })
-        })
+        utl.embed(msg, sMsg, `Роль <@&${role.id}> была забрана у <@${mMember.id}>`)
     }
