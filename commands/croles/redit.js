cbc9ab99fed0c01a84e0ab8943586eda7a1848e6
da-the-dev <leshiktk@me.com>
2021-05-22 22:43:37 +0300

    Role edit part 1

---
+++ commands/croles/redit.js
@@ -0,0 +1,107 @@
+const Discord = require('discord.js')
+const utl = require('../../utility')
+const { DBUser, DBServer } = utl.db
+const sMsg = 'Изменение роли'
+
+module.exports =
+    /**
+    * @param {Array<string>} args Command argument
+    * @param {Discord.Message} msg Discord message object
+    * @param {Discord.Client} client Discord client object
+    * @description Usage: .redit <pos> <edit|name> <value>
+    */
+    async (args, msg, client) => {
+        if(!args[1]) {
+            utl.embed.ping(msg, sMsg, 'не указана роль!')
+            return
+        }
+        if(args[1][0] != 'c' || !Number.isInteger(Number(args[1].slice(1)))) {
+            utl.embed.ping(msg, sMsg, 'указан неверный индекс роли!')
+            return
+        }
+
+        const pos = args[1].slice(1)
+
+        const elements = await Promise.all([
+            new DBServer(msg.guild.id),
+            new DBUser(msg.guild.id, msg.author.id),
+        ])
+        const server = elements[0]
+        const user = elements[1]
+
+        console.log(user.customInv, pos, user.customInv[pos - 1])
+
+        // Check if has roles at all
+        if(!user.customInv) {
+            utl.embed.ping(msg, sMsg, 'у Вас нет кастомных ролей!')
+            return
+        }
+        // If selected role doesn't exist on the server
+        if(!msg.guild.roles.cache.get(user.customInv[pos - 1])) {
+            utl.embed.ping(msg, sMsg, 'такой роли не существует!')
+            // Validate roles
+            user.customInv = user.customInv.filter(r => msg.guild.roles.cache.get(r))
+            user.save()
+            return
+        }
+        // Check out of range
+        const role = user.customInv[pos - 1]
+        if(!role) {
+            utl.embed.ping(msg, sMsg, 'у Вас нет такой кастомной роли!')
+            return
+        }
+        // Check ownership
+        if(!server.customRoles.find(r => r.owner == msg.author.id && r.id == role)) {
+            utl.embed.ping(msg, sMsg, 'эта роль Вам не принадлежит!')
+            return
+        }
+
+        if(!args[2]) {
+            utl.embed.ping(msg, sMsg,
+                `не указано действие!
+                \`\`\`Доступные действия:\n1.name новое имя роли\n2.hex #hex\`\`\``)
+            return
+        }
+
+        switch(args[2]) {
+            case 'name':
+                args.shift()
+                args.shift()
+                args.shift()
+
+                var name = args.join(' ')
+                if(!name) {
+                    utl.embed.ping(msg, sMsg, 'не указано название роли!')
+                    return
+                }
+
+                const discordRole = msg.guild.roles.cache.get(role)
+                discordRole.setName(name, `Изменено ${msg.author} командой .redit`)
+                utl.embed.ping(msg, sMsg, `название роли изменено на ${name}`)
+                break
+            case 'hex':
+                args.shift()
+                args.shift()
+                args.shift()
+
+                var hex = args[0]
+                if(!hex) {
+                    utl.embed.ping(msg, sMsg, 'не указан цвет роли!')
+                    return
+                }
+                if(!hex.startsWith('#') || hex.length != 7) {
+                    utl.embed.ping(msg, sMsg, 'цвет роли должен быть в формате **HEX** и начинаться с `#`!\n```#FFFFFF - Белый\n#000000 - Черный```')
+                    return
+                }
+
+                hex = hex.toUpperCase()
+                hex == '#000000' ? hex == '#010101' : null
+
+                const discordRole = msg.guild.roles.cache.get(role)
+                discordRole.setColor(hex, `Изменено ${msg.author} командой .redit`)
+                utl.embed.ping(msg, sMsg, `цвет роли изменен на ${hex}`)
+                break
+            default:
+                break
+        }
+    }
