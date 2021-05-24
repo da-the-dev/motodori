260cf66dcfb9af22bed2c0baa585beee9cc2fb90
da-the-dev <leshiktk@me.com>
2021-05-24 12:31:02 +0300

    Fixed

--- commands/misc/ugrole.js
+++ commands/misc/ugrole.js
@@ -12,8 +12,8 @@ module.exports =
     */
     async (args, msg, client) => {
         const user = await new DBUser(msg.guild.id, msg.author.id)
-        user.disGameRole = user.disGameRole
+        user.disGameRole = !user.disGameRole
         user.save()
         msg.member.roles.remove(Object.values(constants.gameRoles))
-        utl.embed(msg, sMsg, `Вы успешно ${!user.disGameRole ? '**включили**' : '**отключили**'} роли за игровую активность`)
+        utl.embed(msg, sMsg, `Вы успешно ${user.disGameRole ? '**отключили**' : '**включили**'} роли за игровую активность`)
     }
