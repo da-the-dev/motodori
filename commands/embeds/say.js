7520426dfdbafee81d9c15c53c8f1481d43033d9
da-the-dev <leshiktk@me.com>
2021-05-28 19:33:13 +0300

    Disabled priviliages for some time

--- commands/embeds/say.js
+++ commands/embeds/say.js
@@ -10,39 +10,41 @@ module.exports =
     * @description Usage: .say <jsonData>
     */
     (args, msg, client) => {
-        if(utl.roles.privilage(msg.member, msg.guild.roles.cache.get(constants.roles.curator))) {
-            args = args.join(' ').split('\n')
-            args.shift()
+        // if(utl.roles.privilage(msg.member, msg.guild.roles.cache.get(constants.roles.curator))) {
+        args = args.join(' ').split('\n')
+        args.shift()
 
-            var stringData = args.join('\n')
-            var jsonData = {}
-            if(stringData == "") {
-                utl.embed(msg, sMsg, '\n[EmbedBuilder](https://embedbuilder.nadekobot.me/)\n\n\n\n')
-                return
-            }
-
-            try {
-                jsonData = JSON.parse(stringData)
-            } catch(err) {
-                utl.embed.ping(msg, sMsg, 'Некорректные данные для эмбеда!')
-                return
-            }
-
-            if(jsonData.plainText && Object.keys(jsonData).length == 1) {
-                msg.channel.send(jsonData.plainText)
-            } else {
-                const embed = new Discord.MessageEmbed(jsonData)
-                    .setThumbnail(jsonData.thumbnail)
-                    .setImage(jsonData.image)
-                msg.channel.send({
-                    content: jsonData.plainText,
-                    embed: embed
-                })
-            }
-            // if(jsonData.plainText) msg.channel.send(jsonData.plainText)
-            // else msg.channel.send(embed)
-        } else {
-            utl.embed.ping(msg, '.say', 'у Вас нет прав на эту команду!')
+        var stringData = args.join('\n')
+        var jsonData = {}
+        if(stringData == "") {
+            utl.embed(msg, sMsg, '\n[EmbedBuilder](https://embedbuilder.nadekobot.me/)\n\n\n\n')
+            return
         }
+
+        try {
+            jsonData = JSON.parse(stringData)
+        } catch(err) {
+            utl.embed.ping(msg, sMsg, 'Некорректные данные для эмбеда!')
+            return
+        }
+
+        if(jsonData.plainText && Object.keys(jsonData).length == 1) {
+            msg.channel.send(jsonData.plainText)
+        } else {
+            const embed = new Discord.MessageEmbed(jsonData)
+                .setThumbnail(jsonData.thumbnail)
+                .setImage(jsonData.image)
+            msg.channel.send({
+                content: jsonData.plainText,
+                embed: embed
+            }).catch(err => {
+                msg.channel.send(err)
+            })
+        }
+        // if(jsonData.plainText) msg.channel.send(jsonData.plainText)
+        // else msg.channel.send(embed)
+        // } else {
+        //     utl.embed.ping(msg, '.say', 'у Вас нет прав на эту команду!')
+        // }
     }
 module.exports.allowedInGeneral = true
