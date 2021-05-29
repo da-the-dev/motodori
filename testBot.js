94685259252ca30e33f14299c55d2221dd50960c
da-the-dev <leshiktk@me.com>
2021-05-30 00:59:40 +0300

    Added testBot.js

--- testBot.js
+++ testBot.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
 const { Client, Message, MessageEmbed } = require('discord.js')
 const { embed } = require('./utility')
+const { parser } = require('./tests/events/events')
 
 const guildID = '836297404260155432'
 const myID = process.env.MYID
+const sMsg = 'Создание эвента'
 
 const client = new Client()
 client.login(process.env.BOTTOKEN)
@@ -16,8 +18,85 @@ client.on('message', msg => {
     if(!msg.author.bot && msg.content.startsWith('!test')) {
         var args = msg.content.trim().slice(1).split(" ")
         args.forEach(a => a.trim())
+        args.shift()
 
-        // Stuff happens here
+        try {
+            const evData = parser(args)
+            const emb = new MessageEmbed()
+                .setTitle('⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀Ивент ')
+                .setDescription(`\`\`\`${evData[0]}\`\`\``)
+                .setImage('https://cdn.discordapp.com/attachments/826179756604391536/847877470048878592/f6952822c6ab4c6e72ae1b2a5d00503b.jpg')
+                .setColor('#0xBBECF3')
+
+            if(evData[1]) emb.addField('> Кол-во людей', `\`\`\`${evData[1]}\`\`\``, true)
+            if(evData[2]) emb.addField('> Начало ивента', `\`\`\`${evData[2]}\`\`\``, true)
+            if(evData[3]) emb.addField('> Награда', `\`\`\`${evData[3]}\`\`\``, true)
+
+            msg.channel.send(emb)
+
+        } catch(err) {
+            if(err == 'no args')
+                interactive(msg)
+            else
+                embed(msg, sMsg, `Ошибка: ${err}`)
+        }
     }
 })
 
+/**
+ * 
+ * @param {Message} msg 
+ */
+const interactive = async (msg) => {
+    var name = null
+    var parts = null
+    var start = null
+    var reward = null
+
+    const filter = m => m.author.id == msg.author.id
+    await embed(msg, sMsg, 'Укажите название эвента')
+    await msg.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
+        .then(collected => {
+            name = collected.first()
+        })
+        .catch(() => {
+            embed(msg, sMsg, 'Не указано имя эвента!')
+        })
+
+    await embed(msg, sMsg, 'Укажите количество учасников или `-`')
+    await msg.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
+        .then(collected => {
+            parts = collected.first()
+            const incorrect = embed(msg, sMsg, 'Неверное количество участников!')
+            if(parts == '-') parts = null
+            if(!(parts > 0 && parts < 100 && Number.isInteger(parts)))
+                msg.channel.send(incorrect)
+        })
+        .catch(() => {
+            msg.channel.send(incorrect)
+        })
+
+    await embed(msg, sMsg, 'Укажите время')
+    await msg.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
+        .then(collected => {
+            start = collected.first()
+            const incorrect = embed(msg, sMsg, 'Неверное время!')
+            if(!start || start.length != 5 || start.indexOf(':') == -1 || Number(start.slice(0, 2)) >= 24 || Number(start.slice(3, 5)) >= 60)
+                msg.channel.send(incorrect)
+        })
+        .catch(() => {
+            msg.channel.send(incorrect)
+        })
+
+    await embed(msg, sMsg, 'Укажите время')
+    await msg.channel.awaitMessages(filter, { max: 1, time: 60000, errors: ['time'] })
+        .then(collected => {
+            start = collected.first()
+            const incorrect = embed(msg, sMsg, 'Неверное время!')
+            if(!start || start.length != 5 || start.indexOf(':') == -1 || Number(start.slice(0, 2)) >= 24 || Number(start.slice(3, 5)) >= 60)
+                msg.channel.send(incorrect)
+        })
+        .catch(() => {
+            msg.channel.send(incorrect)
+        })
+}
