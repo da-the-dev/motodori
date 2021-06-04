84fe4b2f3f96a1f862e437be11eefb437c75e5c7
da-the-dev <leshiktk@me.com>
2021-06-04 11:34:04 +0300

    Fix

--- commands/money/br.js
+++ commands/money/br.js
@@ -25,7 +25,7 @@ module.exports =
     async (args, msg, client) => {
         var bet = args[1]
 
-        if(!bet || !Number.isInteger(amount)) {
+        if(!bet || !Number.isInteger(bet)) {
             utl.embed.ping(msg, sMsg, 'не указана ставка!')
             return
         }
