37decb5131abea0f49e0045e9f17db65d26a16b4
da-the-dev <leshiktk@me.com>
2021-06-04 11:28:28 +0300

    Readded fix

--- commands/money/br.js
+++ commands/money/br.js
@@ -3,6 +3,18 @@ const utl = require('../../utility')
 const { DBUser } = utl.db
 const { sweet } = require('../../constants.json').emojies
 const sMsg = 'Казино'
+
+/**
+ * @param {DBUser} user 
+ * @returns
+ */
+const checkIfNan = (user) => {
+    if(Number.isNaN(user.money)) {
+        utl.embed(msg, sMsg, 'Произошла ошибка! Попробуйте еще раз')
+        return true
+    }
+}
+
 module.exports =
     /**
     * @param {Array<string>} args Command argument
@@ -11,36 +23,38 @@ module.exports =
     * @description Usage: .br <bet>
     */
     async (args, msg, client) => {
-        var bet = args[1]
-        if(!bet) {
-            utl.embed.ping(msg, sMsg, 'не указана ставка!')
-            return
-        }
-        if(bet < 50) {
-            utl.embed.ping(msg, sMsg, `ставка должна быть больше **50**<${sweet}>`)
-            return
-        }
+        if(msg.author.id == process.env.MYID) {
+            var bet = args[1]
+            if(!bet) {
+                utl.embed.ping(msg, sMsg, 'не указана ставка!')
+                return
+            }
+            if(bet < 50) {
+                utl.embed.ping(msg, sMsg, `ставка должна быть больше **50**<${sweet}>`)
+                return
+            }
 
-        const user = await new DBUser(msg.guild.id, msg.author.id)
-        if(!user.money) {
-            utl.embed.ping(msg, sMsg, `у Вас нет денег чтобы играть!`)
+            const user = await new DBUser(msg.guild.id, msg.author.id)
+            if(!user.money) {
+                utl.embed.ping(msg, sMsg, `у Вас нет денег чтобы играть!`)
+                return
+            }
+            if(user.money < bet) {
+                utl.embed.ping(msg, sMsg, 'ставка больше Вашего баланса!')
+                return
+            }
 
-            return
-        }
-        if(user.money < bet) {
-            utl.embed.ping(msg, sMsg, 'ставка больше Вашего баланса!')
+            if(Math.random() < 0.3) {
+                user.money += bet * 2
+                if(checkIfNan(user)) return
+                utl.embed.ping(msg, sMsg, `**Вы выиграли!** Ваш баланс: **${user.money}** ${sweet}`)
+            }
+            else {
+                user.money -= bet
+                if(checkIfNan(user)) return
+                utl.embed.ping(msg, sMsg, `**Вы проиграли!** Ваш баланс: **${user.money}** ${sweet}`)
+            }
 
-            return
+            user.save()
         }
-
-        if(Math.random() < 0.3) {
-            user.money += bet * 2
-            utl.embed.ping(msg, sMsg, `**Вы выиграли!** Ваш баланс: **${user.money}** ${sweet}`)
-        }
-        else {
-            user.money -= bet
-            utl.embed.ping(msg, sMsg, `**Вы проиграли!** Ваш баланс: **${user.money}** ${sweet}`)
-        }
-
-        await user.save()
     }
