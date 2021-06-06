d0b038a559b34391c7e04f3d35f36d672da37f0f
da_dev <39898616+da-the-dev@users.noreply.github.com>
2021-06-06 10:40:21 +0300

    Create linkRemover.js

--- utility/linkRemover.js
+++ utility/linkRemover.js
@@ -8,6 +8,7 @@ const regex = /(\.com)|(\.or)(\.ru)|(\.org)|(https:\/\/)|(http:\/\/)/g
  * @param {Message} message 
  */
 module.exports = message => {
+    return 
     const txtControl = message.guild.roles.cache.get(constants.roles.chatControl)
     if(!message.member.roles.cache.find(r => r.position >= txtControl.position)
         && !message.member.roles.cache.has(constants.roles.pics)
