569d6b66f90601451e00015936e4cc2d29ee37e1
da-the-dev <leshiktk@me.com>
2021-06-06 15:36:43 +0300

    Stopped

--- utility/linkRemover.js
+++ utility/linkRemover.js
@@ -8,6 +8,7 @@ const regex = /(\.com)|(\.or)(\.ru)|(\.org)|(https:\/\/)|(http:\/\/)/g
  * @param {Message} message 
  */
 module.exports = message => {
+    return
     const txtControl = message.guild.roles.cache.get(constants.roles.chatControl)
     if(txtControl &&
         !message.member.roles.cache.find(r => r.position >= txtControl.position)
