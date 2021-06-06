61900fa7a5b2cfa1f6f6e518c736a2aa9b2d66fd
da-the-dev <leshiktk@me.com>
2021-06-06 15:43:37 +0300

    Revert "Having a look at what's wrong"
    This reverts commit 9962f7d24aac9c6a9f0f7eab8e19dc1eba3a912b.

--- utility/linkRemover.js
+++ utility/linkRemover.js
@@ -3,19 +3,15 @@ const constants = require('../constants.json')
 
 const regex = /(\.com)|(\.or)(\.ru)|(\.org)|(https:\/\/)|(http:\/\/)/g
 
-module.exports.init = () => {
-    guild.roles.cache.get(constants.roles.chatControl)
-}
 /**
  * Deletes a message if it contains a link
  * @param {Message} message 
  */
 module.exports = message => {
+    return 
     const txtControl = message.guild.roles.cache.get(constants.roles.chatControl)
-    txtControl ? console.log(txtControl.position) : console.log('TXT mod not found!')
-    // const txtControl = message.guild.roles.cache.get(constants.roles.chatControl)
-    // if(!message.member.roles.cache.find(r => r.position >= txtControl.position)
-    //     && !message.member.roles.cache.has(constants.roles.pics)
-    //     && regex.test(message.content))
-    //     message.delete()
+    if(!message.member.roles.cache.find(r => r.position >= txtControl.position)
+        && !message.member.roles.cache.has(constants.roles.pics)
+        && regex.test(message.content))
+        message.delete()
 }
