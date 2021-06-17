10260c5ab49927ce4399dd9a818909ea65a99c46
da-the-dev <leshiktk@me.com>
2021-06-17 22:21:34 +0300

    Disabled link removed

--- utility/linkRemover.js
+++ utility/linkRemover.js
@@ -8,6 +8,7 @@ const regex = /(\.com)|(\.or)(\.ru)|(\.org)|(https:\/\/)|(http:\/\/)/g
  * @param {Message} m 
  */
 module.exports = m => {
+    return
     if(regex.test(m.content))
         if(!m.member.roles.cache.find(r => r.position >= m.guild.roles.cache.get(constants.roles.eventer))
             && !m.member.roles.cache.has(constants.roles.pics))
