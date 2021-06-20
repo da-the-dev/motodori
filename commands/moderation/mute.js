d55e9b737054da5d1f8b88045d9cbda70c4bdd22
da-the-dev <leshiktk@me.com>
2021-06-20 19:20:49 +0300

    Added role check

--- commands/moderation/mute.js
+++ commands/moderation/mute.js
@@ -1,5 +1,5 @@
 const { Message, Client } = require('discord.js')
-const { db, embed, redisConnection, actionLogs } = require('../../utility')
+const { db, embed, redisConnection, actionLogs, roles } = require('../../utility')
 const { DBUser } = db
 const { getRedCon } = redisConnection
 const constants = require('../../constants.json')
@@ -13,68 +13,69 @@ const sMsg = 'Выдача мута'
  * Usage: .mute <member> 5s 10s 5m reason
  */
 module.exports = async (args, msg, client) => {
-    try {
-        const mMember = msg.mentions.members.first()
-        if(!mMember) throw 'no member'
-        args.shift()
-        args.shift()
+    if(roles.privilage(msg.member, msg.guild.roles.cache.get(constants.roles.chatControl)))
+        try {
+            const mMember = msg.mentions.members.first()
+            if(!mMember) throw 'no member'
+            args.shift()
+            args.shift()
 
-        const regex = /\d+s|\d+m|\d+h|\d+d/
+            const regex = /\d+s|\d+m|\d+h|\d+d/
 
-        var time = 0
-        const timeArgs = args.filter(e => regex.test(e))
-        const reason = args.filter(e => !regex.test(e)).join(' ')
-        timeArgs.forEach(e => {
-            if(/\d+s/g.test(e))
-                time += Number(e.slice(0, -1))
-            else if(/\d+m/g.test(e))
-                time += Number(e.slice(0, -1)) * 60
-            if(/\d+h/g.test(e))
-                time += Number(e.slice(0, -1)) * 60 * 60
-            if(/\d+d/g.test(e))
-                time += Number(e.slice(0, -1)) * 60 * 60 * 24
-        })
+            var time = 0
+            const timeArgs = args.filter(e => regex.test(e))
+            const reason = args.filter(e => !regex.test(e)).join(' ')
+            timeArgs.forEach(e => {
+                if(/\d+s/g.test(e))
+                    time += Number(e.slice(0, -1))
+                else if(/\d+m/g.test(e))
+                    time += Number(e.slice(0, -1)) * 60
+                if(/\d+h/g.test(e))
+                    time += Number(e.slice(0, -1)) * 60 * 60
+                if(/\d+d/g.test(e))
+                    time += Number(e.slice(0, -1)) * 60 * 60 * 24
+            })
 
-        if(time <= 0) throw 'invalid time'
-        if(!reason) throw 'invalid reason'
+            if(time <= 0) throw 'invalid time'
+            if(!reason) throw 'invalid reason'
 
-        await getRedCon().set(`mute-${mMember.id}`, '')
-        getRedCon().expire(`mute-${mMember.id}`, time)
+            await getRedCon().set(`mute-${mMember.id}`, '')
+            getRedCon().expire(`mute-${mMember.id}`, time)
 
-        const user = await new DBUser(mMember.guild.id, mMember.id)
-        user.mute = true
-        user.save()
+            const user = await new DBUser(mMember.guild.id, mMember.id)
+            user.mute = true
+            user.save()
 
-        mMember.roles.add(constants.roles.muted)
+            mMember.roles.add(constants.roles.muted)
 
-        const duration = require('moment').duration(time, 'seconds');
-        const mmD = duration.days()
-        const mmH = duration.hours()
-        const mmM = duration.minutes()
-        const mmS = duration.seconds()
+            const duration = require('moment').duration(time, 'seconds');
+            const mmD = duration.days()
+            const mmH = duration.hours()
+            const mmM = duration.minutes()
+            const mmS = duration.seconds()
 
-        var muteMsg = ''
+            var muteMsg = ''
 
-        if(mmD) muteMsg += `**${mmD.toString()}**д `
-        if(mmH) muteMsg += `**${mmH.toString()}**ч `
-        if(mmM) muteMsg += `**${mmM.toString()}**м `
-        if(mmS) muteMsg += `**${mmS.toString()}**с `
+            if(mmD) muteMsg += `**${mmD.toString()}**д `
+            if(mmH) muteMsg += `**${mmH.toString()}**ч `
+            if(mmM) muteMsg += `**${mmM.toString()}**м `
+            if(mmS) muteMsg += `**${mmS.toString()}**с `
 
-        muteMsg = muteMsg.trim()
-
-        embed(msg, sMsg, `<@${mMember.user.id}> получил(-а) **мут** на ${muteMsg} \n\`\`\`Elm\nПричина: ${reason}\n\`\`\``)
-        actionLogs.modLog(client.guild, 'mute', msg.member, mMember, msg.createdTimestamp, reason, time)
-    } catch(err) {
-        switch(err) {
-            case 'no member':
-                embed.ping(msg, sMsg, `не указан участник!`)
-                break
-            case 'invalid time':
-                embed.ping(msg, sMsg, `неверно указано время!`)
-                break
-            case 'invalid reason':
-                embed.ping(msg, sMsg, `неверно указана причина!`)
-                break
+            muteMsg = muteMsg.trim()
+            console.log('here')
+            embed(msg, sMsg, `<@${mMember.user.id}> получил(-а) **мут** на ${muteMsg} \n\`\`\`Elm\nПричина: ${reason}\n\`\`\``)
+            actionLogs.modLog(client.guild, 'mute', msg.member, mMember, msg.createdTimestamp, reason, time)
+        } catch(err) {
+            switch(err) {
+                case 'no member':
+                    embed.ping(msg, sMsg, `не указан участник!`)
+                    break
+                case 'invalid time':
+                    embed.ping(msg, sMsg, `неверно указано время!`)
+                    break
+                case 'invalid reason':
+                    embed.ping(msg, sMsg, `неверно указана причина!`)
+                    break
+            }
         }
-    }
 }
